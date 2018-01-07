5fcf1efc01e81943708df3df86e5d8e9d85e9033
Tarik Amar <tarik.amar@gmail.com>
2018-01-07 16:43:57 +0100

    User - model - method to get data user from his id

--- src/module/user/UserModel.php
+++ src/module/user/UserModel.php
@@ -2,12 +2,44 @@
 
 namespace Module\User;
 
+use Exception,
+    RuntimeException,
+    PDO;
+
 class UserModel
 {
+    private $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
     public function getInfoFromId(int $id)
     {
-        //TODO sql query
+        $id = intval($id, 10);
 
-        return ['id' => $id, 'name' => 'tarik', 'email' => 'tarik@e.mail'];
+        $query = "SELECT id, name, email FROM user WHERE id=$id;";
+
+        try {
+            $result = $this->db->query($query);
+        } catch (Exception $e){
+            throw new RuntimeException(
+                'Unable to execute query',
+                intval($e->getCode(), 10),
+                $e
+            );
+        }
+
+        $resultFetched = $result->fetch(PDO::FETCH_OBJ);
+
+        if (empty($resultFetched))
+            return null;
+
+        return [
+            'id'    => $resultFetched->id,
+            'name'  => $resultFetched->name,
+            'email' => $resultFetched->email
+        ];
     }
 }
--- src/module/user/UserModelTest.php
+++ src/module/user/UserModelTest.php
@@ -5,10 +5,53 @@ use PHPUnit\Framework\TestCase,
 
 class UserModelTest extends TestCase
 {
-    public function testGetInfoFromId()
+    public function testGetInfoFromIdException()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Unable to execute query');
+
+        $id = 11;
+        $pdoMock = $this->createMock(PDO::class);
+        $pdoMock->expects($this->once())
+                ->method('query')
+                ->with("SELECT id, name, email FROM user WHERE id=$id;")
+                ->will($this->throwException(new Exception));
+
+        (new UserModel($pdoMock))->getInfoFromId($id);
+    }
+
+    public function testGetInfoFromIdDataNotFound()
+    {
+        $id = 22;
+
+        $pdoStatementMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock = $this->createMock(PDO::class);
+        $pdoMock->expects($this->once())
+                ->method('query')
+                ->with("SELECT id, name, email FROM user WHERE id=$id;")
+                ->willReturn($pdoStatementMock);
+
+        $this->assertNull((new UserModel($pdoMock))->getInfoFromId($id));
+    }
+
+    public function testGetInfoFromIdDataFound()
     {
         $id = 33;
-        $infoFromId = (new UserModel())->getInfoFromId($id);
-        $this->assertSame(['id' => $id, 'name' => 'tarik', 'email' => 'tarik@e.mail'], $infoFromId);
+        $dataToRetrieve = ['id' => 0, 'name' => 'foo', 'email' => 'bar'];
+
+        $pdoStatementMock = $this->createMock(PDOStatement::class);
+        $pdoStatementMock->expects($this->once())
+                ->method('fetch')
+                ->with(PDO::FETCH_OBJ)
+                ->willReturn((object) $dataToRetrieve);
+
+        $pdoMock = $this->createMock(PDO::class);
+        $pdoMock->expects($this->once())
+                ->method('query')
+                ->with("SELECT id, name, email FROM user WHERE id=$id;")
+                ->willReturn($pdoStatementMock);
+
+        $this->assertEquals($dataToRetrieve, (new UserModel($pdoMock))->getInfoFromId($id));
     }
 }
